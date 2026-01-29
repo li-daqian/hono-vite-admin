618ecdc537ee20e9537eefcab76ca3d25a079efa
li-daqian <lidaqian11@gmail.com>
2026-01-29 15:16:47 +0800

    feat: install pinia-plugin-persistedstate

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -2,6 +2,7 @@ import App from '@admin/App.vue'
 import { setupAxios } from '@admin/lib/axios'
 import router from '@admin/router'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { createApp } from 'vue'
 import './style.css'
 
@@ -9,7 +10,10 @@ setupAxios()
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
+
 app.use(router)
 
 app.mount('#app')
