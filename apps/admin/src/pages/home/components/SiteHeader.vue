b97f3b44a6b73df5ff7cb43809797755d5e08b3c
li-daqian <lidaqian11@gmail.com>
2026-02-10 14:58:33 +0800

    feat: make logo area do not shrink

--- apps/admin/src/pages/home/components/SiteHeader.vue
+++ apps/admin/src/pages/home/components/SiteHeader.vue
@@ -17,25 +17,23 @@ const logoAreaWidth = computed(() => {
 </script>
 
 <template>
-  <div class="flex items-center bg-background h-(--header-height) w-full sticky top-0 z-50 border-b">
-    <header class="flex items-center px-4 w-full">
-      <div :class="`${logoAreaWidth}`">
-        <a class="flex items-center gap-2 select-none">
-          <Logo />
-          <span class="text-lg font-semibold">User Admin</span>
-        </a>
-      </div>
-      <div>
-        <SidebarTrigger :size="18" class="rounded-lg cursor-pointer size-8" />
-      </div>
-      <div class="flex items-center gap-2 ml-auto">
-        <Button variant="ghost" size="icon" class="rounded-full cursor-pointer">
-          <ToggleTheme class="text-lg" />
-        </Button>
-        <Button variant="ghost" size="icon-lg" class="rounded-full cursor-pointer">
-          <UserNav />
-        </Button>
-      </div>
-    </header>
+  <div class="flex items-center bg-background h-(--header-height) w-full sticky top-0 z-50 border-b px-4">
+    <div :class="`${logoAreaWidth}`" class="shrink-0">
+      <a class="flex items-center gap-2 select-none whitespace-nowrap">
+        <Logo />
+        <span class="text-lg font-semibold">User Admin</span>
+      </a>
+    </div>
+    <div>
+      <SidebarTrigger :size="18" class="rounded-lg cursor-pointer size-8" />
+    </div>
+    <div class="flex items-center gap-2 ml-auto">
+      <Button variant="ghost" size="icon" class="rounded-full cursor-pointer">
+        <ToggleTheme class="text-lg" />
+      </Button>
+      <Button variant="ghost" size="icon-lg" class="rounded-full cursor-pointer">
+        <UserNav />
+      </Button>
+    </div>
   </div>
 </template>
