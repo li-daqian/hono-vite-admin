5ff256abed173c708e356b8bad49f41f1e8f7cb8
li-daqian <lidaqian11@gmail.com>
2026-02-11 18:46:23 +0800

    refactor: move log.vue

--- apps/admin/src/pages/home/components/SiteHeader.vue
+++ apps/admin/src/pages/home/components/SiteHeader.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import Logo from '@admin/components/Logo.vue'
 import ToggleTheme from '@admin/components/ToggleTheme.vue'
 import { Button } from '@admin/components/ui/button'
 import { SidebarTrigger } from '@admin/components/ui/sidebar'
 import { SIDEBAR_WIDTH } from '@admin/components/ui/sidebar/utils'
 import { cn } from '@admin/lib/utils'
+import AppLogo from '@admin/pages/home/components/AppLogo.vue'
 import UserNav from '@admin/pages/home/components/UserNav.vue'
 </script>
 
@@ -14,7 +14,7 @@ import UserNav from '@admin/pages/home/components/UserNav.vue'
       <a class="flex items-center gap-4 px-4">
         <SidebarTrigger :size="18" class="rounded-lg cursor-pointer" />
         <div class="flex items-center justify-center gap-2">
-          <Logo class="size-6" />
+          <AppLogo class="size-6" />
           <span class="text-lg font-semibold">User Admin</span>
         </div>
       </a>
