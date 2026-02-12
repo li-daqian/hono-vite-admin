ab45bd8eab502ae6873303a1b51825af4a38664d
li-daqian <lidaqian11@gmail.com>
2026-02-12 14:49:06 +0800

    fix: improve logo area width handling in SiteHeader component

--- apps/admin/src/pages/home/components/SiteHeader.vue
+++ apps/admin/src/pages/home/components/SiteHeader.vue
@@ -3,26 +3,31 @@ import AppLogo from '@admin/components/AppLogo.vue'
 import ToggleTheme from '@admin/components/ToggleTheme.vue'
 import { Button } from '@admin/components/ui/button'
 import { SidebarTrigger } from '@admin/components/ui/sidebar'
-import { SIDEBAR_WIDTH } from '@admin/components/ui/sidebar/utils'
+import { SIDEBAR_WIDTH, useSidebar } from '@admin/components/ui/sidebar/utils'
 import AppBreadcrumb from '@admin/pages/home/components/AppBreadcrumb.vue'
 import UserNav from '@admin/pages/home/components/UserNav.vue'
 import { useMenuStore } from '@admin/stores/menu'
 import { computed } from 'vue'
 
 const menuStore = useMenuStore()
+const sidebar = useSidebar()
 
 const breadcrumb = computed(() => {
   return menuStore.breadcrumb
+})
+
+const logoAreaWidth = computed(() => {
+  return sidebar.isMobile.value ? 'auto' : SIDEBAR_WIDTH
 })
 </script>
 
 <template>
   <div class="flex items-center bg-background h-(--header-height) w-full sticky top-0 z-50 border-b shrink-0 text-nowrap">
-    <div :style="{ width: SIDEBAR_WIDTH }" class="flex shrink-0 items-center gap-4 px-4">
+    <div :style="{ width: `${logoAreaWidth}` }" class="flex shrink-0 items-center gap-4 pl-4">
       <SidebarTrigger :size="18" class="rounded-lg cursor-pointer" />
       <div class="flex items-center justify-center gap-2">
         <AppLogo class="size-6" />
-        <span class="text-lg font-semibold">User Admin</span>
+        <span class="text-lg font-semibold leading-none hidden md:block">User Admin</span>
       </div>
     </div>
     <AppBreadcrumb :items="breadcrumb" class="px-4" />
