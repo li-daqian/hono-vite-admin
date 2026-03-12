b98d04601ddbe43fa609aadab93db926a8ede794
Li Daqian <hi@lidaqian.me>
2026-03-12 12:26:56 +0800

    refactor: change user route order

--- apps/server/src/modules/user/user.route.ts
+++ apps/server/src/modules/user/user.route.ts
@@ -17,9 +17,9 @@ import {
 } from '@server/src/modules/user/user.openapi'
 
 export function userRoute(api: OpenAPIHono) {
-  api.openapi(createUserRoute, handleCreateUser)
   api.openapi(getUserProfileRoute, handleGetUserProfile)
   api.openapi(getUserPageRoute, handleGetUserPage)
+  api.openapi(createUserRoute, handleCreateUser)
   api.openapi(updateUserRoute, handleUpdateUser)
   api.openapi(deleteUsersBatchRoute, handleDeleteUsersBatch)
   api.openapi(updateUserStatusBatchRoute, handleUpdateUserStatusBatch)
