00ed2fb82ea38c6163510ebd723d3e8d2f8d38bb
Li Daqian <hi@lidaqian.me>
2026-03-13 17:28:09 +0800

    refactor: simplify UsersProvider by using Pinia store for state management

--- apps/admin/src/pages/users/component/users-provider.vue
+++ apps/admin/src/pages/users/component/users-provider.vue
@@ -1,53 +1,24 @@
 <script lang="ts">
 import type { GetUserPageResponse } from '@admin/client'
-import type { InjectionKey, Ref } from 'vue'
-import { defineComponent, inject, provide, ref } from 'vue'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
 type User = GetUserPageResponse['items'][number]
 
 export type UsersDialogType = 'add' | 'edit' | 'delete'
 
-export interface UsersContextType {
-  open: Ref<UsersDialogType | null>
-  setOpen: (value: UsersDialogType | null) => void
-  currentRow: Ref<User | null>
-  setCurrentRow: (value: User | null) => void
-}
+export const useUsersStore = defineStore('users', () => {
+  const open = ref<UsersDialogType | null>(null)
+  const currentRow = ref<User | null>(null)
 
-export const USERS_CONTEXT_KEY: InjectionKey<UsersContextType> = Symbol('USERS_CONTEXT_KEY')
-
-export function useUsers() {
-  const usersContext = inject(USERS_CONTEXT_KEY)
-
-  if (!usersContext) {
-    throw new Error('useUsers has to be used within <UsersProvider>')
+  function setOpen(value: UsersDialogType | null) {
+    open.value = value
   }
 
-  return usersContext
-}
+  function setCurrentRow(value: User | null) {
+    currentRow.value = value
+  }
 
-export default defineComponent({
-  name: 'UsersProvider',
-  setup(_, { slots }) {
-    const open = ref<UsersDialogType | null>(null)
-    const currentRow = ref<User | null>(null)
-
-    const setOpen = (value: UsersDialogType | null) => {
-      open.value = value
-    }
-
-    const setCurrentRow = (value: User | null) => {
-      currentRow.value = value
-    }
-
-    provide(USERS_CONTEXT_KEY, {
-      open,
-      setOpen,
-      currentRow,
-      setCurrentRow,
-    })
-
-    return () => slots.default?.()
-  },
+  return { open, setOpen, currentRow, setCurrentRow }
 })
 </script>
